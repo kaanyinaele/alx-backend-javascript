0ffb8b8d3b4527f67c20fc8253601ecc85d0ae2a
kaanyinaele <kaanyinaele@gmail.com>
2024-07-26 11:31:43 +0100

    fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
+// 0-promise.js
 export default function getResponseFromAPI() {
-  return new Promise(() => {});
+    return new Promise((resolve, reject) => {
+        resolve("API response");
+    });
 }
+

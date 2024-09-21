34de94a4f2b00c5f18038fa8781c7539818afee8
kaanyinaele <kaanyinaele@gmail.com>
2024-09-21 11:17:15 +0100

    hooks

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,8 +1,8 @@
 const Utils = require("./utils");
 
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const totalCost = Utils.calculateNumber("SUM", totalAmount, totalShipping);
   console.log(`The total is: ${totalCost}`);
-};
+}
 
 module.exports = sendPaymentRequestToApi;
